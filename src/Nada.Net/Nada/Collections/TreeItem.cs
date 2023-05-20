4d043a187e987c5dcba7e350535470cab0c905de
Thomas Ley <thomas.ley@cgi.com>
2023-05-20 10:01:46 +0200

    Dictionary extensions added Linq extensions added List to Tree extensions added. Partially tests added

---
+++ src/Nada.Net/Nada/Collections/TreeItem.cs
@@ -0,0 +1,7 @@
+﻿namespace Nada.Collections;
+
+public class TreeItem<T>
+{
+    public T? Item { get; set; }
+    public IEnumerable<TreeItem<T>>? Children { get; set; }
+}
---
+++ src/Nada.Net/Nada/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace MagicBox.Extensions
+{
+    public static class DictionaryExtensions
+    {
+        public static void AddOrUpdate<TKey, TValue>(this IDictionary<TKey, TValue> source, TKey key, TValue value )
+        {
+            if (source.ContainsKey(key))
+            {
+                source[key] = value;
+            }
+            else
+            {
+                source.Add(key, value);
+            }
+        }
+    }
+}
