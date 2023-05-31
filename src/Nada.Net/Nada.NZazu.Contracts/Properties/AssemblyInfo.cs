920ae06889ae1cd3efdeaef39fc9ae627ba748ff
Thomas Ley <thomas.ley@cgi.com>
2023-05-31 14:43:24 +0200

    fixing some nuget packaging.

--- src/Nada.Net/Nada.NZazu.Contracts/Properties/AssemblyInfo.cs
+++ src/Nada.Net/Nada.NZazu.Contracts/Properties/AssemblyInfo.cs
@@ -2,6 +2,5 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("NZazu.Contracts")]
 [assembly: Guid("3f633a54-aee6-4a07-adf9-c9d1990d6927")]
 [assembly: InternalsVisibleTo("Nada.NZazu.Contracts.Tests")]
